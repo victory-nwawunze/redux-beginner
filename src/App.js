c32abffabaaa8f450322d47af1067393fee47d0b
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-27 09:00:09 -0700

    implementing redux function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 /** @format */
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+  const addBy = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,19 @@
 
 import { createStore } from "redux";
 
-const reducerFn = (state = { counter: 10 }, action) => {
+const reducerFn = (state = { counter: 0 }, action) => {
+  // Synchronous function
+  // we should not mutitate the original state
+
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+  if (action.type === "ADD") {
+    return { counter: state.counter + action.payload };
+  }
   return state;
 };
 
