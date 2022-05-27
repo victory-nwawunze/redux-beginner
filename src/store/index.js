b8467157e442aa6a4c6b365c3bc7a0c951bb8376
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-27 08:38:15 -0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import { createStore } from "redux";
+
+const reducerFn = (state = { counter: 10 }, action) => {
+  return state;
+};
+
+const store = createStore(reducerFn);
+
+export default store;
